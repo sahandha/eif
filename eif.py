392cc30e60f1c1d482fd49d1b6647ff6ebacc409
Sahand Hariri <sahandha@gmail.com>
2018-09-27 11:55:00 -0500

    Converted docs to docstring format

--- eif.py
+++ eif.py
@@ -1,4 +1,9 @@
-    "Extended Isolation forest functions"
+""" Extended Isolation forest functions
+
+This is the implementation of the Extended Isolation Forest anomaly detection algorithm. This extension, improves the consistency and reliability of the anomaly score produced by standard Isolation Forest represented by Liu et al.
+Our method allows for the slicing of the data to be done using hyperplanes with random slopes which results in improved score maps. The consistency and reliability of the algorithm is much improved using this extension.
+
+"""
 
 __author__ = 'Matias Carrasco Kind & Sahand Hariri'
 import numpy as np
@@ -12,19 +17,52 @@ def c_factor(n) :
     return 2.0*(np.log(n-1)+0.5772156649) - (2.0*(n-1.)/(n*1.0))
 
 class iForest(object):
-    '''
+    """
     Creates an iForest object. This object holds the data as well as the trained trees (iTree objects).
-    '''
+
+    Attributes
+    ----------
+    X : list
+        Data used for training. It is a list of list of floats.
+    nobjs: int
+        Size of the dataset.
+    sample: int
+        Size of the sample to be used for tree creation.
+    Trees: list
+        A list of tree objects.
+    limit: int
+        Maximum depth a tree can have.
+    exlevel: int
+        Exention level to be used in the creating splitting critera.
+    c: float
+        Multiplicative factor used in computing the anomaly scores.
+
+    Methods
+    -------
+    CheckExtensionLevel()
+        Chaeck the validity of extension level provided by user based on the data
+    compute_paths(X_in)
+        Computes the anomaly score for data X_in
+    """
     def __init__(self, X, ntrees,  sample_size, limit=None, ExtensionLevel=0):
-        '''
+        """
         iForest(X, ntrees,  sample_size, limit=None, ExtensionLevel=0)
         Initialize a forest by passing in training data, number of trees to be used and the subsample size.
-            * X - Training data. List of [x1,x2,...,xn] coordinate points.
-            * ntrees - Number of trees to be used. Integer value.
-            * sample_size - The size of the subsample to be used in creation of each tree. Integer. Must be smaller than |X|
-            * limit - The maximum allowed tree depth. This is by default set to average length of unsucessful search in a binary tree. Integer.
-            * ExtensionLevel - Specifies degree of freedom in choosing the hyperplanes for dividing up data. Integer. Must be smaller than the dimension n of the dataset.
-        '''
+
+        Parameters
+        ----------
+        X : list of list of floats
+            Training data. List of [x1,x2,...,xn] coordinate points.
+        ntrees : int
+            Number of trees to be used.
+        sample_size : int
+            The size of the subsample to be used in creation of each tree. Must be smaller than |X|
+        limit : int
+            The maximum allowed tree depth. This is by default set to average length of unsucessful search in a binary tree.
+        ExtensionLevel : int
+            Specifies degree of freedom in choosing the hyperplanes for dividing up data. Must be smaller than the dimension n of the dataset.
+        """
+
         self.ntrees = ntrees
         self.X = X
         self.nobjs = len(X)
@@ -36,15 +74,15 @@ class iForest(object):
         if limit is None:
             self.limit = int(np.ceil(np.log2(self.sample)))                     # Set limit to the default as specified by the paper (average depth of unsuccesful search through a binary tree).
         self.c = c_factor(self.sample)
-        for i in range(self.ntrees):                                            # This look builds an ensemble of iTrees (the forest).
+        for i in range(self.ntrees):                                            # This loop builds an ensemble of iTrees (the forest).
             ix = rn.sample(range(self.nobjs), self.sample)
             X_p = X[ix]
             self.Trees.append(iTree(X_p, 0, self.limit, exlevel=self.exlevel))
 
     def CheckExtensionLevel(self):
-        '''
+        """
         This function makes sure the extension level provided by the user does not exceed the dimension of the data. An exception will be raised in the case of a violation.
-        '''
+        """
         dim = self.X.shape[1]
         if self.exlevel < 0:
             raise Exception("Extension level has to be an integer between 0 and "+ str(dim-1)+".")
@@ -52,11 +90,20 @@ class iForest(object):
             raise Exception("Your data has "+ str(dim) + " dimensions. Extension level can't be higher than " + str(dim-1) + ".")
 
     def compute_paths(self, X_in = None):
-        '''
+        """
         compute_paths(X_in = None)
         Compute anomaly scores for all data points in a dataset X_in
-            * X_in - Data to be scored. iForest.Trees are used for computing the depth reached in each tree by each data point.
-        '''
+
+        Parameters
+        ----------
+        X_in : list of list of floats
+                Data to be scored. iForest.Trees are used for computing the depth reached in each tree by each data point.
+
+        Returns
+        -------
+        float
+            Anomaly score for a given data point.
+        """
         if X_in is None:
             X_in = self.X
         S = np.zeros(len(X_in))
@@ -69,20 +116,48 @@ class iForest(object):
         return S
 
 class Node(object):
-    '''
+    """
     A single node from each tree (each iTree object). Nodes containe information on hyperplanes used for data division, date to be passed to left and right nodes, whether they are external or internal nodes.
-    '''
+
+    Attributes
+    ----------
+    e: int
+        Depth of the tree to which the node belongs.
+    size: int
+        Size of the dataset present at the node.
+    X: list
+        Data at the node.
+    n: list
+        Normal vector used to build the hyperplane that splits the data in the node.
+    p: list
+        Intercept point through which the hyperplane passes.
+    lef: Node object
+        Left child node.
+    right: Node object
+        Right child node.
+    ntype: str
+        The type of the node: 'exNode', 'inNode'.
+    """
     def __init__(self, X, n, p, e, left, right, node_type = '' ):
-        '''
+        """
         Node(X, n, p, e, left, right, node_type = '' )
         Create a node in a given tree (iTree objectg)
-            * X - Training data available to each node. List of [x1,x2,...,xn] coordinate points.
-            * n - Normal vector for the hyperplane used for splitting data. List of floats.
-            * p - Intercept point for the hyperplane used for splitting data. List of floats.
-            * left - Left child node. A Node object.
-            * right - Right child node. A Node object.
-            * node_type - Specifies if the node is external or internal. String. Takes two values: 'exNode', 'inNode'.
-        '''
+
+        Parameters
+        ----------
+        X : list of list of floats
+            Training data available to each node. List of [x1,x2,...,xn] coordinate points.
+        n : list of floats
+            Normal vector for the hyperplane used for splitting data.
+        p : list of floats
+            Intercept point for the hyperplane used for splitting data.
+        left : Node object
+            Left child node.
+        right : Node object
+            Right child node.
+        node_type : str
+            Specifies if the node is external or internal. Takes two values: 'exNode', 'inNode'.
+        """
         self.e = e
         self.size = len(X)
         self.X = X # to be removed
@@ -96,17 +171,54 @@ class iTree(object):
 
     """
     A single tree in the forest that is build using a unique subsample.
+
+    Attributes
+    ----------
+    exlevel: int
+        Extension level used in the splitting criteria.
+    e: int
+        Depth of tree
+    X: list
+        Data present at the root node of this tree.
+    size: int
+        Size of the dataset.
+    dim: int
+        Dimension of the dataset.
+    Q: list
+        List of ordered integers smaller than dim.
+    l: int
+        Maxium depth a tree can reach before its creation is terminated.
+    n: list
+        Normal vector at the root of this tree, which is used in creating hyperplanes for splitting critera
+    p: list
+        Intercept point at the root of this tree through which the splitting hyperplane passes.
+    exnodes: int
+        The number of external nodes this tree has.
+    root: Node object
+        At each node create a new tree.
+
+    Methods
+    -------
+    make_tree(X, e, l)
+        Builds the tree recursively from a given node. Returns a Node object.
     """
 
     def __init__(self,X,e,l, exlevel=0):
-        '''
+        """
         iTree(X, e, l, exlevel=0)
         Create a tree
-            * X - Subsample of training data. |X| = iForest.sample_size. List of [x1,x2,...,xn] coordinate points
-            * e - Depth of the tree as it is being traversed down. Integer. e <= l.
-            * l - The maximum depth the tree can reach before its creation is terminated. Integer.
-            * exlevel - Specifies degree of freedom in choosing the hyperplanes for dividing up data. Integer. Must be smaller than the dimension n of the dataset.
-        '''
+
+        Parameters
+        ----------
+        X : list of list of floats
+            Subsample of training data. |X| = iForest.sample_size. List of [x1,x2,...,xn] coordinate points
+        e : int
+            Depth of the tree as it is being traversed down. e <= l.
+        l : int
+            The maximum depth the tree can reach before its creation is terminated.
+        exlevel : int
+            Specifies degree of freedom in choosing the hyperplanes for dividing up data. Must be smaller than the dimension n of the dataset.
+        """
         self.exlevel = exlevel
         self.e = e
         self.X = X                                                              #save data for now. Not really necessary.
@@ -120,13 +232,23 @@ class iTree(object):
         self.root = self.make_tree(X,e,l)                                       # At each node create a new tree, starting with root node.
 
     def make_tree(self,X,e,l):
-        '''
+        """
         make_tree(X,e,l)
         Builds the tree recursively from a given node. Returns a Node object.
-            * X Subsample of training data. |X| = iForest.sample_size. List of [x1,x2,...,xn] coordinate point.
-            * e - Depth of the tree as it is being traversed down. Integer. e <= l.
-            * l - The maximum depth the tree can reach before its creation is terminated. Integer.
-        '''
+
+        Parameters
+        ----------
+        X: list of list of floats
+            Subsample of training data. |X| = iForest.sample_size. List of [x1,x2,...,xn] coordinate point.
+        e : int
+            Depth of the tree as it is being traversed down. Integer. e <= l.
+        l : int
+            The maximum depth the tree can reach before its creation is terminated. Integer.
+
+        Returns
+        -------
+        Node object
+        """
         self.e = e
         if e >= l or len(X) <= 1:                                               # A point is isolated in traning data, or the depth limit has been reached.
             left = None
@@ -140,34 +262,62 @@ class iTree(object):
             self.n = np.random.normal(0,1,self.dim)                             # A random normal vector picked form a uniform n-sphere. Note that in order to pick uniformly from n-sphere, we need to pick a random normal for each component of this vector.
             self.n[idxs] = 0
             self.p = np.random.uniform(mins,maxs)                               # Picking a random intercept point for the hyperplane splitting data.
-            w = (X-self.p).dot(self.n) < 0                                      # Criteria that determines if a data point should go to the left or right child node. 
+            w = (X-self.p).dot(self.n) < 0                                      # Criteria that determines if a data point should go to the left or right child node.
             return Node(X, self.n, self.p, e,\
             left=self.make_tree(X[w],e+1,l),\
             right=self.make_tree(X[~w],e+1,l),\
             node_type = 'inNode' )
 
 class PathFactor(object):
-    '''
+    """
     Given a single tree (iTree objext) and a data point x = [x1,x2,...,xn], compute the legth of the path traversed by the point on the tree when it reaches an external node.
-    '''
+
+    Attributes
+    ----------
+    path_list: list
+        A list of strings 'L' or 'R' which traces the path a data point travels down a tree.
+    x: list
+        A single data point, which is represented as a list of floats.
+    e: int
+        The depth of a given node in the tree.
+
+    Methods
+    -------
+    find_path(T)
+        Given a tree, it finds the path a single data points takes.
+
+    """
     def __init__(self,x,itree):
-        '''
+        """
         PathFactor(x, itree)
         Given a single tree (iTree objext) and a data point x = [x1,x2,...,xn], compute the legth of the path traversed by the point on the tree when it reaches an external node.
-            * x - A data point x = [x1, x2, ..., xn].
-            * itree - An iTree object.
-        '''
+
+        Parameters
+        ----------
+        x : list of floats
+            A data point x = [x1, x2, ..., xn].
+        itree : iTree object
+            A single tree.
+        """
         self.path_list=[]
         self.x = x
         self.e = 0
         self.path  = self.find_path(itree.root)
 
     def find_path(self,T):
-        '''
+        """
         find_path(T)
         Given a tree, find the path for a single data point based on the splitting criteria stored at each node.
-            * T - An iTree object.
-        '''
+
+        Parameters
+        ----------
+        T : iTree object
+
+        Returns
+        -------
+        int
+            The depth reached by the data point.
+        """
         if T.ntype == 'exNode':
             if T.size <= 1: return self.e
             else:
@@ -187,9 +337,18 @@ class PathFactor(object):
                 return self.find_path(T.right)
 
 def all_branches(node, current=[], branches = None):
-    '''
+    """
     Utility function used in generating a graph visualization. It returns all the branches of a given tree so they can be visualized.
-    '''
+
+    Parameters
+    ----------
+    node: Node object
+
+    Returns
+    -------
+    list
+        list of branches that were reached.
+    """
     current = current[:node.e]
     if branches is None: branches = []
     if node.ntype == 'inNode':
