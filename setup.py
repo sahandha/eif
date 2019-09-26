b8c2800e0aa20144e18fb40f4e03f4296cdf0f4a
Matias Carrasco Kind <mgckind@gmail.com>
2019-09-25 23:36:54 -0500

    refactoring to cxx

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 import sys
 import os
+import numpy
+from Cython.Distutils import build_ext
 try:
     from setuptools import setup, find_packages
+    from setuptools.extension import Extension
 except ImportError:
     from distutils.core import setup
+    from distutils.extension import Extension
 prjdir = os.path.dirname(__file__)
+
 
 def read(filename):
     return open(os.path.join(prjdir, filename)).read()
+
 
 extra_link_args = []
 libraries = []
@@ -19,8 +25,14 @@ setup(
     version=__version__,
     author='Matias Carrasco Kind , Sahand Hariri',
     author_email='mcarras2@illinois.edu , sahandha@gmail.com',
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[Extension("eif",
+                 sources=["_eif.pyx", "eif.cxx"],
+                 include_dirs=[numpy.get_include()],
+                 extra_compile_args=['-Wcpp'],
+                 language="c++")],
     scripts=[],
-    py_modules=['eif','version'],
+    py_modules=['eifb', 'version'],
     packages=[],
     license='License.txt',
     description='Extended Isolation Forest for anomaly detection',
